97efcf2e8022b8a4cbe4e5af01c5506129a82b09
citizenzet <exgamer@live.ru>
2019-11-12 17:52:29 +0600

    к

--- src/views/banner-url-link/update.php
+++ src/views/banner-url-link/update.php
@@ -6,7 +6,7 @@ $this->title = Yii::t('banner', 'Редактировать баннер на с
     'name' => $model->url,
 ]);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('banner', 'Баннеры и страницы'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $originModel->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->url, 'url' => ['view', 'id' => $originModel->id]];
 $this->params['breadcrumbs'][] = Yii::t('banner', 'Редактировать');
 ?>
 <div class="post-category-update">
