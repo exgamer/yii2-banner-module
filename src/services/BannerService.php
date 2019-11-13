71b6e3c7fb2f083cb566575d25e4f57a3601b3c1
citizenzet <exgamer@live.ru>
2019-11-13 17:15:54 +0600

    к

--- src/services/BannerService.php
+++ src/services/BannerService.php
@@ -6,7 +6,8 @@ use concepture\yii2logic\services\Service;
 use Yii;
 use concepture\yii2logic\services\traits\StatusTrait;
 use concepture\yii2logic\services\traits\LocalizedReadTrait;
-
+use yii\db\ActiveQuery;
+use concepture\yii2logic\enum\StatusEnum;
 
 /**
  * Class BannerService
@@ -21,5 +22,22 @@ class BannerService extends Service
     protected function beforeCreate(Form $form)
     {
         $form->user_id = Yii::$app->user->identity->id;
+    }
+
+    /**
+     * Возвращает баннеры для текущего url по хешу md5 url
+     *
+     * @return array
+     */
+    public function getBannersForCurrentUrl()
+    {
+        $current = Yii::$app->getRequest()->getPathInfo();
+        $md5 = md5($current);
+
+        return $this->getAllByCondition(function(ActiveQuery $query) use($md5) {
+            $query->innerJoinWith('urlLinks');
+            $query->andWhere("u.url_md5_hash = :url_md5_hash", [':url_md5_hash' => $md5]);
+            $query->andWhere("status = :status", [':status' => StatusEnum::ACTIVE]);
+        });
     }
 }
