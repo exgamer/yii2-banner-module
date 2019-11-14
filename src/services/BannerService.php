bb5f2cb3b6d4bd4ffe258e98da264b692fde71cd
citizenzet <exgamer@live.ru>
2019-11-14 18:29:50 +0600

    к

--- src/services/BannerService.php
+++ src/services/BannerService.php
@@ -33,6 +33,13 @@ class BannerService extends Service
     {
         $current = Yii::$app->getRequest()->getPathInfo();
         $md5 = md5($current);
+        $modelClass = $this->getRelatedModelClass();
+        $modelClass::$search_by_locale_callable = function($q, $localizedAlias) {
+            $q->orWhere("{$localizedAlias}.from_at is null and {$localizedAlias}.to_at is null");
+            $q->orWhere("now() between {$localizedAlias}.from_at and {$localizedAlias}.to_at and {$localizedAlias}.from_at is not null and {$localizedAlias}.to_at is not null");
+            $q->orWhere("{$localizedAlias}.from_at < now() and {$localizedAlias}.from_at is not null and {$localizedAlias}.to_at is  null");
+            $q->orWhere("{$localizedAlias}.to_at > now() and {$localizedAlias}.from_at is  null and {$localizedAlias}.to_at is not null");
+        };
 
         return $this->getAllByCondition(function(ActiveQuery $query) use($md5) {
             $query->innerJoinWith('urlLinks');
